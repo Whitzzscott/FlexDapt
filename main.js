5199df59105ae3e3baff9b8550b9b2ffa355e46d
Whitzzscott <samytplayz197@gmail.com>
2024-12-01 16:52:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 
 //ignore this as this is for sending the user to the version according to their device to avoid conflict with elements
-if (document.getElementById('mobile')) {
-    document.getElementById('mobile').addEventListener('click', () => {
-        const creatingUrl1 = 'mobile.html';
-        window.open(creatingUrl1, '_blank', 'width=600,height=400');
+if (document.getElementById('pc')) {
+    document.getElementById('pc').addEventListener('click', () => {
+        const creatingUrl = 'mobile.html';
+        window.location.href = creatingUrl;
     });
 }
+
 
 if (document.getElementById('pc')) {
     document.getElementById('pc').addEventListener('click', () => {
